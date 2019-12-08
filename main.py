d5647b89a8b76a0e88332ce88efa90580c334913
eg7eg7 <eg7eg7@gmail.com>
2019-12-08 13:24:29 +0200

    fixed layers

--- main.py
+++ main.py
@@ -1,38 +1,42 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
-WITH_LAYER = False
+WITH_LAYER = True
 
 
 def main():
-    T = 0.1
+    T = 1
+    n_input = 784  # 28x28
+    n_output = 10
+    n_hidden1 = 200
+    n_hidden2 = 200
     mnist = input_data.read_data_sets("MNIST_DATA", one_hot=True)
 
     if WITH_LAYER:
-        x = tf.compat.v1.placeholder(tf.float32, [None, 784], name="Inputs")
-        t = tf.compat.v1.placeholder(tf.float32, [None, 10], name="Targets")
+        x = tf.compat.v1.placeholder(tf.float32, [None, n_input], name="Inputs")
+        t = tf.compat.v1.placeholder(tf.float32, [None, n_output], name="Targets")
 
-        h1 = tf.Variable(tf.zeros([784, 200]), name="h1")
-        b1 = tf.Variable(tf.zeros([200]), name="b1")
-        h2 = tf.Variable(tf.zeros([200, 200]), name="h2")
-        b2 = tf.Variable(tf.zeros([200]), name="b2")
+        h1 = tf.Variable(tf.zeros([n_input, n_hidden1]), name="h1")
+        b1 = tf.Variable(tf.zeros([1, n_hidden1]), name="b1")
+        h2 = tf.Variable(tf.zeros([n_hidden1, n_hidden2]), name="h2")
+        b2 = tf.Variable(tf.zeros([1, n_hidden2]), name="b2")
 
-        w = tf.Variable(tf.zeros([200, 10]), name="Out_layer_w")
-        b = tf.Variable(tf.zeros([10]), name="Out_biases")
+        w = tf.Variable(tf.zeros([n_hidden2, n_output]), name="Out_layer_w")
+        b = tf.Variable(tf.zeros([1, n_output]), name="Out_biases")
 
         h1_s = tf.matmul(x, h1) + b1
-        h1_s_zig = tf.sigmoid(h1_s / T)
+        h1_s_zig = tf.nn.relu(h1_s / T)
         h2_s = tf.matmul(h1_s_zig, h2) + b2
-        h2_s_zig = tf.sigmoid(h2_s / T)
+        h2_s_zig = tf.nn.relu(h2_s / T)
         z = tf.matmul(h2_s_zig, w) + b
-        y = tf.sigmoid(z / T)
+        y = tf.nn.relu(z / T)
 
     else:
-        x = tf.compat.v1.placeholder(tf.float32, [None, 784], name="Inputs")
-        t = tf.compat.v1.placeholder(tf.float32, [None, 10], name="Targets")
+        x = tf.compat.v1.placeholder(tf.float32, [None, n_input], name="Inputs")
+        t = tf.compat.v1.placeholder(tf.float32, [None, n_output], name="Targets")
 
-        w = tf.Variable(tf.zeros([784, 10]), name="Out_layer_w")
-        b = tf.Variable(tf.zeros([10]), name="Out_biases")
+        w = tf.Variable(tf.zeros([n_input, n_output]), name="Out_layer_w")
+        b = tf.Variable(tf.zeros([n_output]), name="Out_biases")
 
         z = tf.matmul(x, w) + b
         y = tf.sigmoid(z/T)
@@ -46,14 +50,14 @@ def main():
     sess = tf.compat.v1.InteractiveSession()
     tf.compat.v1.global_variables_initializer().run()
 
-    batch_xsx, batch_ys = mnist.train.next_batch(batch_size=100)
+    # batch_xsx, batch_ys = mnist.train.next_batch(batch_size=100)
 
     # for x, y in zip(batch_xsx, batch_ys):
     #    print("x= " + str(x)," y=" + str(y))
 
     for _ in range(13000):
-        batch_xsx, batch_ts = mnist.train.next_batch(50)
-        print(sess.run([train_step, cross_entropy], feed_dict={x: batch_xsx, t:batch_ts}))
+        batch_xsx, batch_ts = mnist.train.next_batch(100)
+        print(sess.run([train_step, cross_entropy], feed_dict={x: batch_xsx, t: batch_ts}))
     correct_prediction = tf.equal(tf.math.argmax(y, 1), tf.math.argmax(t, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
