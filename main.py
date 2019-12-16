9e049c50b0b9d7532e69f85042d818621ce3e254
eg7eg7 <eg7eg7@gmail.com>
2019-12-16 14:56:58 +0200

    refactor

--- main.py
+++ main.py
@@ -9,16 +9,12 @@ T = 1  # TODO consider removing it
 # TODO display number of weights in architecture
 
 
-def logistic_regression_with_layer(n_input=784, n_output=10, n_hidden1=200, n_hidden2=200, training_range=13000, batch_size=50):
+def logistic_regression_with_layer(n_input=784, n_output=10, n_hidden1=200, n_hidden2=200):
     logging.info("***********LOGISTIC REGRESSION WITH LAYER BEGIN***********")
-    #logging.info("layer = " + str(with_layer) + " input size = " + str(n_input) + " output size = " + str(n_output))
     logging.info("Layer 1 size = " + str(n_hidden1) + " Layer 2 size = " + str(n_hidden2))
-    logging.info("training range = " + str(training_range) + " batch size = " + str(batch_size))
-    seed = tf.set_random_seed(random.randint(1, 1000))
-    mnist = input_data.read_data_sets("MNIST_DATA", one_hot=True)
+    seed = tf.compat.v1.set_random_seed(random.randint(1, 1000))
 
     x = tf.compat.v1.placeholder(tf.float32, [None, n_input], name="Inputs")
-    t = tf.compat.v1.placeholder(tf.float32, [None, n_output], name="Targets")
 
     h1 = tf.Variable(tf.random.uniform([n_input, n_hidden1], -1, 1, seed=seed), name="h1")
     b1 = tf.Variable(tf.random.uniform([1, n_hidden1], -1, 1, seed=seed), name="b1")
@@ -34,55 +30,28 @@ def logistic_regression_with_layer(n_input=784, n_output=10, n_hidden1=200, n_hi
     h2_s_rel = tf.nn.relu(h2_s / T)
     z = tf.add(tf.matmul(h2_s_rel, w), b)
 
-    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=t, logits=z))
-    train_step = tf.compat.v1.train.AdamOptimizer(name="Adam").minimize(cross_entropy)
-
-    sess = tf.compat.v1.InteractiveSession()
     tf.compat.v1.global_variables_initializer().run()
 
-    for _ in range(training_range):
-        batch_xsx, batch_ts = mnist.train.next_batch(batch_size=batch_size)
-        ts, ce = sess.run([train_step, cross_entropy], feed_dict={x: batch_xsx, t: batch_ts})
-    correct_prediction = tf.equal(tf.math.argmax(z, 1), tf.math.argmax(t, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    logging.info("Accuracy with training data = " + str(sess.run(accuracy, feed_dict={x: batch_xsx, t: batch_ts})))
-    logging.info("Accuracy with test data = " + str(sess.run(accuracy, feed_dict={x: mnist.test.images, t: mnist.test.labels})))
-    sess.close()
-    return w, b, z
+    return x, z
 
 
-def logistic_regression(n_input=784, n_output=10,training_range=13000, batch_size=50):
+def logistic_regression(n_input=784, n_output=10):
     logging.info("***********LOGISTIC REGRESSION BEGIN***********")
     logging.info("input size = " + str(n_input) + " output size = " + str(n_output))
-    logging.info("training range = " + str(training_range) + " batch size = " + str(batch_size))
-    seed = tf.set_random_seed(random.randint(1, 1000))
-    mnist = input_data.read_data_sets("MNIST_DATA", one_hot=True)
+    seed = tf.compat.v1.set_random_seed(random.randint(1, 1000))
     x = tf.compat.v1.placeholder(tf.float32, [None, n_input], name="Inputs")
     t = tf.compat.v1.placeholder(tf.float32, [None, n_output], name="Targets")
     w = tf.Variable(tf.random.uniform([n_input, n_output], -1, 1, seed=seed), name="Out_layer_w")
     b = tf.Variable(tf.random.uniform([n_output], -1, 1, seed=seed), name="Out_biases")
     z = tf.add(tf.matmul(x, w), b)
 
-    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=t, logits=z))
-    train_step = tf.compat.v1.train.AdamOptimizer(name="Adam").minimize(cross_entropy)
-
-    sess = tf.compat.v1.InteractiveSession()
     tf.compat.v1.global_variables_initializer().run()
 
-    for _ in range(training_range):
-        batch_xsx, batch_ts = mnist.train.next_batch(batch_size=batch_size)
-        ts, ce = sess.run([train_step, cross_entropy], feed_dict={x: batch_xsx, t: batch_ts})
-    correct_prediction = tf.equal(tf.math.argmax(z, 1), tf.math.argmax(t, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    logging.info("Accuracy with training data = " + str(sess.run(accuracy, feed_dict={x: batch_xsx, t: batch_ts})))
-    logging.info("Accuracy with test data = " + str(sess.run(accuracy, feed_dict={x: mnist.test.images, t: mnist.test.labels})))
-    sess.close()
-    return w, b, z
+    return x, z
 
 
 def weight_variable(shape):
-    initial = tf.random.truncated_normal(shape=shape, stddev=0.1, mean=0.0, dtype=tf.dtypes.float32,
-                                         seed=None, name="random_truncated_norm")
+    initial = tf.random.truncated_normal(shape=shape, stddev=0.1)
     return tf.Variable(initial)
 
 
@@ -149,17 +118,40 @@ def logistic_regression_conv_layers(x_input_size=28, y_input_size=28, n_input=78
     print("Test accuracy %g" % accuracy.eval(feed_dict={x: mnist.test.images, t: mnist.test, keep_prob: drop_rate_percent}))
 
 
-def buildTrain(train_func):
-    return train_func()
+def build_train(network, n_output=10, training_range=13000, batch_size=50):
+    sess = tf.compat.v1.InteractiveSession()
+    x, z = network()
+    mnist = input_data.read_data_sets("MNIST_DATA", one_hot=True)
+
+    x, z = train_network(session=sess, input_x=x, output_z=z, n_output=n_output, training_range=training_range, batch_size=batch_size, data=mnist)
+    sess.close()
+    return x, z
+
+
+def train_network(session, input_x, output_z, n_output, training_range, batch_size, data):
+    t = tf.compat.v1.placeholder(tf.float32, [None, n_output], name="Targets")
+    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=t, logits=output_z))
+    train_step = tf.compat.v1.train.AdamOptimizer(name="Adam").minimize(cross_entropy)
+    for _ in range(training_range):
+        batch_xsx, batch_ts = data.train.next_batch(batch_size=batch_size)
+        ts, ce = session.run([train_step, cross_entropy], feed_dict={input_x: batch_xsx, t: batch_ts})
+    correct_prediction = tf.equal(tf.math.argmax(output_z, 1), tf.math.argmax(t, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    logging.info("Accuracy with training data = " + str(session.run(accuracy, feed_dict={input_x: batch_xsx, target_t: batch_ts})))
+    logging.info("Accuracy with test data = " + str(session.run(accuracy, feed_dict={input_x: data.test.images, target_t: data.test.labels})))
+    return input_x, output_z
 
 
 def main():
-    w, b, z = buildTrain(logistic_regression)
-    w, b, z = buildTrain(logistic_regression_with_layer)
-   # w, b, z = buildTrain(logistic_regression_conv_layers)
-    # logistic_regression(with_layer=False)
-    # logistic_regression(with_layer=True)
-    #logistic_regression_conv_layers()
+    x, z = build_train(network=logistic_regression)
+    #x, z = build_train(network=logistic_regression_with_layer)
+   #  #w, b, z = buildTrain(logistic_regression)
+   # # w, b, z = buildTrain(logistic_regression_with_layer)
+   # # w, b, z = buildTrain(logistic_regression_conv_layers)
+   #  # logistic_regression(with_layer=False)
+   #  # logistic_regression(with_layer=True)
+   #  #logistic_regression_conv_layers()
+   #  logistic_regression_conv_layers()
 
 
 if __name__ == "__main__":
